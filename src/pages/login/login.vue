28abbe9d7fc77456cbb9a3c58c43db5a56d5c82b
wangshanshan <1162667156@qq.com>
2020-08-07 17:17:07 +0800

    调整

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -33,8 +33,8 @@
 <script>
   export default {
     name: 'login',
-    data: {
-      return () {}
+    data () {
+      return {}
     },
     methods: {}
   }
@@ -43,129 +43,113 @@
 <style lang="scss" scoped>
   @import '../../assets/scss/login.scss';
 
-  @media (max-width: 768px) {
-    .login-box {
-      width: 100%;
-
-      .checkbox {
-        text-align: left;
-        margin-top: 8px;
-        display: flex;
-        align-items: center;
-        color: #99999A;
-        height: 18px;
-
-        label {
-          font-size: 14px;
-          line-height: 29px;
-        }
-      }
-
-      .button {
-        display: inline-block;
+   @media (max-width: 768px) {
+     .login-box {
         width: 89%;
-        height: 52px;
-        line-height: 52px;
-        border-radius: 16px;
-        background: #5A7786;
-        border: none;
-        margin-top: 8px;
-        color: #fff;
-        font-size: 12px;
-
-        img {
-          width: 7%;
-          top: 15%;
+         .checkbox {
+            margin-top: 34px;
+            height: 60px;
+             label {
+                font-size: 28px;
+                line-height: 60px;
+                padding-left: 60px;
+             }
+            label::before {
+                width: 59px;
+                height: 59px;
+                line-height: 59px;
+                border-radius: 14px;
+            }
+            label::after {
+                width: 59px;
+                height: 59px;
+                line-height: 59px;
+                border-radius: 14px;
+            }
+         }
+         .forget {
+            font-size: 28px;
+            color: #878787;
+            line-height: 58px;
+            margin: 34px 0 38px;
         }
-      }
-
-      .forget {
-        font-size: 8px;
-        color: #878787;
-        line-height: 6px;
-        margin: 10px 0 43px;
-      }
+        .or{
+            fieldset{
+                height: 56px;
+                border-top: 4px solid;
+                legend{
+                    padding: 0 40px;
+                    font-size: 40px;
+                }
+            }
+         }
+     }
+ }
+ @media (min-width: 769px) {
+     .login-box {
+        width: 70%;
+         .checkbox {
+             margin-top: 8px;
+             height: 18px;
+             label {
+                font-size: 8px;
+                line-height: 17px;
+             }
+            label::before {
+                width: 17px;
+                height: 17px;
+                line-height: 17px;
+            }
+            label::after {
+                width: 17px;
+                height: 17px;
+                line-height: 17px;
+            }
+         }
+         .forget {
+             font-size: 10px;
+             line-height: 6px;
+             margin: 10px 0;
+         }
+        .or{
+            fieldset{
+                height:20px;
+                border-top: 1px solid;
+                legend{
+                    padding: 0 10px;
+                    font-size: 12px;
+               }
+            }
+        }
+     }
+ }
+ .login-box {
+     margin: 0 auto;
+    color: #99999A;
+     .facebook {
+        background: #3966B8;
+        border-right: none;
+        border-bottom: none;
+        color: #DBDBE5;
+        border-color: #DBDBE5;
+        legend{
+            text-align: center;
+            color: #747474;
+            opacity:0.49;
+        }
     }
-  }
-
-  @media (min-width: 769px) {
-    .login-box {
-      width: 73%;
-
-      .checkbox {
-        margin-top: 8px;
-        height: 18px;
-
-        label {
-          font-size: 8px;
-          line-height: 17px;
-        }
-      }
-
-      .button {
-        width: 100%;
-        height: 27px;
-        line-height: 27px;
-        border-radius: 3px;
-        margin-top: 8px;
-        font-size: 10px;
-
-        img {
-          width: 7%;
-          top: 15%;
-        }
-      }
-
-      .login-btn {
-        font-size: 12px;
-      }
-
-      .forget {
-        font-size: 10px;
-        line-height: 6px;
-        margin: 10px 0;
-      }
-    }
-  }
-
-  .login-box {
-    margin: 0 auto;
-
     .email {
       margin-top: 0;
     }
-
     .checkbox {
       text-align: left;
       display: flex;
       align-items: center;
       color: #99999A;
-
-      label {}
     }
-
-    .button {
-      display: inline-block;
-      border: none;
-      color: #fff;
-      position: relative;
-
-      img {
-        display: inline-block;
-        float: left;
-        position: absolute;
-        left: 4%;
-      }
-    }
-
-    .login-btn {
-      background: #5A7786;
-    }
-
     .facebook {
       background: #3966B8;
     }
-
     .google {
       background: #DEDEDE;
     }
@@ -173,21 +157,15 @@
     .forget {
       color: #878787;
     }
-
     .or {
       fieldset {
         border-left: none;
         border-right: none;
         border-bottom: none;
         color: #DBDBE5;
-        border-color: #defffc;
-        height: 20px;
-        border-top: 1px solid;
-
+        border-color: #DBDBE5;
         legend {
           text-align: center;
-          padding: 0 10px;
-          font-size: 12px;
           color: #747474;
           opacity: 0.49;
         }
